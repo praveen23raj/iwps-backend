6cfcf441f8ee1e97afd8172eca4ac39d4979721d
praveen <praveenvinod6@gmail.com>
2026-09-14 22:34:23 +0530

    h2 db

--- src/main/java/com/iwps/controller/TestController.java
+++ src/main/java/com/iwps/controller/TestController.java
@@ -1,4 +1,16 @@
 package com.iwps.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
 public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running";
+    }
 }
+
